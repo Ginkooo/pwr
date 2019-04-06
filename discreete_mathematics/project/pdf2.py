790592e986b7e2bbea1a9246b4f0e4c06301aabe
Piotr Czajka <piotr_czajka@outlook.com>
2019-04-06 22:41:47 +0200

    sdfs

--- discreete_mathematics/project/pdf2.py
+++ discreete_mathematics/project/pdf2.py
@@ -1,16 +1,51 @@
-from dataclasses import dataclass
-import string
+from itertools import count, islice
+from random import choice, randint
 
 
-@dataclass
-class Node:
-    visited: bool = False
+distances = {
+    'B': {'A': 5, 'D': 1, 'G': 2},
+    'A': {'B': 5, 'D': 3, 'E': 12, 'F': 5},
+    'D': {'B': 1, 'G': 1, 'E': 1, 'A': 3},
+    'G': {'B': 2, 'D': 1, 'C': 2},
+    'C': {'G': 2, 'E': 1, 'F': 16},
+    'E': {'A': 12, 'D': 1, 'C': 1, 'F': 2},
+    'F': {'A': 5, 'E': 2, 'C': 16}}
 
 
-p_idx = string.ascii_lowercase.find('p')
+def distance_generator():
+    pow_gen = (10**i for i in islice(count(1), 4))
+    for node_count in pow_gen:
+        nodes = [x for x in range(node_count)]
+        distances = {}
+        for node in nodes:
+            distances[node] = {choice(nodes): randint(1, 10) for _ in range(int(len(nodes) * 0.2))}
+        print('dupa')
+        yield distances
 
-node_names = string.ascii_lowercase[:p_idx+1]
-for name in node_names:
-    globals()[name] = Node()
+for distances in distance_generator():
+    continue
 
-unvisited = {}
+    nodes = tuple(sorted(tuple(distances)))
+
+
+    unvisited = {node: None for node in nodes}
+    visited = {}
+    current = 0
+    currentDistance = 0
+    unvisited[current] = currentDistance
+
+    while True:
+        for neighbour, distance in distances[current].items():
+            if neighbour not in unvisited:
+                continue
+            newDistance = currentDistance + distance
+            if unvisited[neighbour] is None or unvisited[neighbour] > newDistance:
+                unvisited[neighbour] = newDistance
+        visited[current] = currentDistance
+        del unvisited[current]
+        if not unvisited:
+            break
+        candidates = [node for node in unvisited.items() if node[1]]
+        current, currentDistance = sorted(candidates, key=lambda x: x[1])[0]
+
+    print(visited)
